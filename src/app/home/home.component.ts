36491f52bf5c00001d4a3be08819e80a605ac6f3
nvcastelli <nick.v.castelli@gmail.com>
2019-07-15 10:30:35 -0500

    base changes bringing to master

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.isLoading = true;
     this.quoteService
-      .getRandomQuote({ category: 'dev' })
+      .getMessages({ category: 'dev' })
       .pipe(
         finalize(() => {
           this.isLoading = false;
